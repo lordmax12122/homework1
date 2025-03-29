a29801930759c9d0a6324e568de8c122137557f1
lordmax12122 <116733872+lordmax12122@users.noreply.github.com>
2025-03-29 22:16:13 +0200

    done

---
+++ index.js
@@ -0,0 +1,108 @@
+const galleryList = document.querySelector(".gallery")
+const backdrop = document.querySelector(".lightbox")
+const modal = document.querySelector(".lightbox-modal")
+const originalImage = document.querySelector(".lightbox__image")
+const modalClose = document.querySelector(".lightbox__button")
+
+const galleryItems = [
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg',
+      description: 'Hokkaido Flower',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+      description: 'Container Haulage Freight',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+      description: 'Aerial Beach View',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+      description: 'Flower Blooms',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+      description: 'Alpine Mountains',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+      description: 'Mountain Lake Sailing',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+      description: 'Alpine Spring Meadows',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+      description: 'Nature Landscape',
+    },
+    {
+      preview:
+        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+      original:
+        'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+      description: 'Lighthouse Coast Sea',
+    },
+  ];
+
+  const galleryPhotos = galleryItems
+  .map((galleryPhoto) => {
+    const gallery = `<li class="gallery-item">
+        <img class="gallery-image" src="${galleryPhoto.preview}" data-source="${galleryPhoto.original}" alt="${galleryPhoto.description}" />
+      </li>`;
+    return gallery;
+  })
+  .join("");
+  galleryList.innerHTML = galleryPhotos;
+
+  galleryList.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "IMG") {
+        return
+    }
+    modal.classList.remove("is-hidden");
+  });
+
+  galleryList.addEventListener('click', event => {
+    originalImage.src = galleryItems[Array.from(galleryList.getElementsByTagName("img")).indexOf(event.target)].original
+  })
+
+  modalClose.addEventListener("click", (event) => {
+    modal.classList.add("is-hidden")
+  })
+
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        modal.classList.add("is-hidden")
+    }
+  })
+
+
+  
+
+
+
